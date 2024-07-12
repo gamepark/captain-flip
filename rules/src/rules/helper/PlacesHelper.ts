6de688f9125233cb350ee0c0b8d3d819d207c567
Patrick Beaume <p.beaume@gmail.com>
2024-07-12 08:53:59 +0200

    Fix end of game triggering

--- rules/src/rules/helper/PlacesHelper.ts
+++ rules/src/rules/helper/PlacesHelper.ts
@@ -3,6 +3,8 @@ import { BoardADescription } from '../../material/board/description/BoardADescri
 import { LocationType } from '../../material/LocationType'
 import { MaterialType } from '../../material/MaterialType'
 import { PlayerId } from '../../PlayerId'
+import maxBy from 'lodash/maxBy'
+import minBy from 'lodash/minBy'
 
 export class PlacesHelper extends MaterialRulesPart {
   constructor(game: MaterialGame, readonly player: PlayerId) {
@@ -28,19 +30,17 @@ export class PlacesHelper extends MaterialRulesPart {
       if (this.isColumnFull(x)) count++
     }
 
-    return count === 4
+    return count >= 4
   }
 
   isColumnFull(column: number) {
     const boardDescription = this.boardDescription
+    const columnPlaces = boardDescription.places.filter((p) => p.x === column)
+    const maxY = maxBy(columnPlaces, (place) => place.y)!.y
+    const minY = minBy(columnPlaces, (place) => place.y)!.y
+    const maxTileCount = (maxY - minY) + 1
     const tiles = this.boardTile.filter((item) => item.location.x === column)
-    const availablePlaces: Record<number, number> = {}
-    for (const place of boardDescription.places) {
-      if (place.x in availablePlaces && place.y < availablePlaces[place.x]) continue
-      const occupiedPlace = tiles.filter((item) => item.location.x === place.x && item.location.y === place.y).length > 0
-      if (occupiedPlace) availablePlaces[place.x] = place.y
-    }
-    return availablePlaces
+    return tiles.length === maxTileCount
   }
 
   get boardDescription() {
