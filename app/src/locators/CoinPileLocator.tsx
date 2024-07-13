d3e52d5e7b07d7fec1d6a4a2e1ff9404204ef625
Patrick Beaume <p.beaume@gmail.com>
2024-07-13 13:04:29 +0200

    handle stock location properly

--- app/src/locators/CoinPileLocator.tsx
+++ app/src/locators/CoinPileLocator.tsx
@@ -1,9 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import { PileLocator } from '@gamepark/react-game'
 import { MaterialItem } from '@gamepark/rules-api/dist/material/items/MaterialItem'
+import { coin10StockLocation, coin1StockLocation, coin3StockLocation, coin5StockLocation } from '../material/CoinDescription'
 import { CoinPileDescription } from './descriptions/CoinPileDescription'
 
 export class CoinPileLocator extends PileLocator {
+  locations = [
+    coin1StockLocation,
+    coin3StockLocation,
+    coin5StockLocation,
+    coin10StockLocation
+  ]
   locationDescription = new CoinPileDescription()
   radius = 3
   coordinates = { x: 0, y: 0, z: 0 }
--- app/src/material/CoinDescription.tsx
+++ app/src/material/CoinDescription.tsx
@@ -1,11 +1,12 @@
 /** @jsxImportSource @emotion/react */
 import { Coin } from '@gamepark/captain-flip/material/Coin'
+import { LocationType } from '@gamepark/captain-flip/material/LocationType'
 import { TokenDescription } from '@gamepark/react-game'
+import { MaterialItem } from '@gamepark/rules-api/dist/material/items/MaterialItem'
 import Coin1 from '../images/coins/Coin1.png'
+import Coin10 from '../images/coins/Coin10.png'
 import Coin3 from '../images/coins/Coin3.png'
 import Coin5 from '../images/coins/Coin5.png'
-import Coin10 from '../images/coins/Coin10.png'
-import { coinStockLocation } from '../locators/descriptions/CoinPileDescription'
 
 export class CoinDescription extends TokenDescription {
   height = 2.79
@@ -20,14 +21,30 @@ export class CoinDescription extends TokenDescription {
 
 
   staticItems = [
-    { id: Coin.Coin10, quantity: 10, location: coinStockLocation },
-    { id: Coin.Coin1, quantity: 10, location: coinStockLocation },
-    { id: Coin.Coin5, quantity: 10, location: coinStockLocation },
-    { id: Coin.Coin3, quantity: 10, location: coinStockLocation },
+    { id: Coin.Coin10, quantity: 10, location: coin10StockLocation },
+    { id: Coin.Coin5, quantity: 10, location: coin5StockLocation },
+    { id: Coin.Coin3, quantity: 10, location: coin3StockLocation },
+    { id: Coin.Coin1, quantity: 10, location: coin1StockLocation },
   ]
 
-  stockLocation = coinStockLocation
-
+  getStockLocation(item: MaterialItem) {
+    switch(item.id) {
+      case Coin.Coin1:
+        return coin1StockLocation;
+      case Coin.Coin3:
+        return coin3StockLocation;
+      case Coin.Coin5:
+        return coin5StockLocation;
+      default:
+      case Coin.Coin10:
+        return coin10StockLocation;
+    }
+  }
 }
+
+export const coin1StockLocation = { type: LocationType.CoinStock, id: Coin.Coin1 }
+export const coin3StockLocation = { type: LocationType.CoinStock, id: Coin.Coin3 }
+export const coin5StockLocation = { type: LocationType.CoinStock, id: Coin.Coin5 }
+export const coin10StockLocation = { type: LocationType.CoinStock, id: Coin.Coin10 }
 
 export const coinDescription = new CoinDescription()
