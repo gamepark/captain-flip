c2c32fb274cf06b93c7e706c1bc95a3d2499ad43
Patrick Beaume <p.beaume@gmail.com>
2024-07-18 16:03:29 +0200

    place player coins on board

--- app/src/locators/PlayerCoinLocator.tsx
+++ app/src/locators/PlayerCoinLocator.tsx
@@ -1,29 +1,28 @@
 /** @jsxImportSource @emotion/react */
 import { Coin } from '@gamepark/captain-flip/material/Coin'
-import { getRelativePlayerIndex, ItemContext, PileLocator } from '@gamepark/react-game'
-import { MaterialItem } from '@gamepark/rules-api'
-import { adventureBoardLocation } from './AdventureBoardLocator'
+import { MaterialType } from '@gamepark/captain-flip/material/MaterialType'
+import { ItemContext, PileLocator } from '@gamepark/react-game'
+import { Location, MaterialItem } from '@gamepark/rules-api'
 
 
 export class PlayerCoinLocator extends PileLocator {
-  radius = 2
+  parentItemType = MaterialType.AdventureBoard
+  radius = 1.5
 
+  getParentItem(location: Location, context: ItemContext) {
+    return context.rules.material(MaterialType.AdventureBoard).player(location.player).getItem()!
+  }
 
-  getCoordinates(item: MaterialItem, context: ItemContext) {
-    const coordinates = adventureBoardLocation.getBoardPosition(item.location.player!, context)
-
-
-    const index = getRelativePlayerIndex(context, item.location.player!)
-    if (context.rules.players.length === 2) {
-      coordinates.x += (index === 0? 10: -10)
-      coordinates.y -= 14.1
-    } else if (context.rules.players.length === 5 && index === 2) {
-      coordinates.x += 14.1
-      coordinates.y += 10
-    } else {
-      coordinates.x += coordinates.x < 0? 14.1: -14.1
-      coordinates.y -= 10
+  getPositionOnParent() {
+    return {
+      x: 87,
+      y: 10
     }
+  }
+
+
+  getCoordinates(item: MaterialItem) {
+    const coordinates = { x: 0, y: 0, z: 0.05 }
 
     if (item.id === Coin.Coin5) coordinates.z = 1
     if (item.id === Coin.Coin3) coordinates.z = 1.5
