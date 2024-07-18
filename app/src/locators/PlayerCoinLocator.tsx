c7d1e7a33bea144aad0eef0a5818e38b58ca69b2
Patrick Beaume <p.beaume@gmail.com>
2024-07-18 15:27:52 +0200

    improve player coin hand position

--- app/src/locators/PlayerCoinLocator.tsx
+++ app/src/locators/PlayerCoinLocator.tsx
@@ -1,11 +1,12 @@
 /** @jsxImportSource @emotion/react */
+import { Coin } from '@gamepark/captain-flip/material/Coin'
 import { getRelativePlayerIndex, ItemContext, PileLocator } from '@gamepark/react-game'
 import { MaterialItem } from '@gamepark/rules-api'
 import { adventureBoardLocation } from './AdventureBoardLocator'
 
 
 export class PlayerCoinLocator extends PileLocator {
-  radius = 1.5
+  radius = 2
 
 
   getCoordinates(item: MaterialItem, context: ItemContext) {
@@ -23,6 +24,10 @@ export class PlayerCoinLocator extends PileLocator {
       coordinates.x += coordinates.x < 0? 14.1: -14.1
       coordinates.y -= 10
     }
+
+    if (item.id === Coin.Coin5) coordinates.z = 1
+    if (item.id === Coin.Coin3) coordinates.z = 1.5
+    if (item.id === Coin.Coin1) coordinates.z = 2
 
     return coordinates
   }
